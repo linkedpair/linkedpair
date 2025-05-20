a603a2642bf19ffeb82c5ad310135c1fb83d606d
William Liow <williamoose@mac.lan>
2025-05-20 16:05:20 +0800

    added some features

--- frontend/screens/SignUpScreen.js
+++ frontend/screens/SignUpScreen.js
@@ -5,14 +5,19 @@ import {
   View,
   StyleSheet,
   TextInput,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  Image
 } from 'react-native';
 
 export default function SignUpScreen({ navigation }) {
   
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [male, setMale] = useState(null)
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [hidePassword, setHidePassword] = useState(true);
   
   const handleSignIn = () => {
     console.log("username:", username);
@@ -24,30 +29,60 @@ export default function SignUpScreen({ navigation }) {
   return (
     <KeyboardAvoidingView style={styles.MainContainer}>
       <View style={styles.WhiteSpace} />
-      <View style={styles.WhiteSpace}>
+      <View style={styles.FormContainer}>
         <Text style={styles.SignUpText}>Sign Up</Text>
-        <Typeable 
-        type="Username" 
-        value={username}
-        onChangeText={setUsername}
+        <View style={styles.NameRow}>
+          <NameInput
+            type="First Name"
+            value={firstName}
+            onChangeText={setFirstName}
+          />
+          <NameInput
+            type="Last Name"
+            value={lastName}
+            onChangeText={setLastName}
+          />
+        </View>
+        <View style={styles.GenderRow}>
+          <MaleButton
+            selected={male}
+            onPress={() => {
+              setMale(true)
+              alert(`I am male`)
+            }}
+          />
+          <FemaleButton 
+            selected={male === false}
+            onPress={() => {
+              setMale(false)
+              alert(`I am female`)
+            }}
+          />
+        </View>
+        <StandardInput 
+          type="Username" 
+          value={username}
+          onChangeText={setUsername}
         />
-        <Typeable 
-        type="Email"
-        value={email}
-        onChangeText={setEmail}
+        <StandardInput 
+          type="Email"
+          value={email}
+          onChangeText={setEmail}
         />
-        <Typeable 
-        type="Password"
-        value={password}
-        onChangeText={setPassword}
+        <PasswordInput 
+          type="Password"
+          value={password}
+          onChangeText={setPassword}
+          hidePassword={hidePassword}
+          setHidePassword={setHidePassword}
         />
         <SignUpButton onPress={handleSignIn}/>
         <View style={styles.linkContainer}>
         <Text>Already Have an Account? {" "}
           <Text 
-          style={styles.RedirectToSignInText}
-          onPress={() => navigation.navigate("SignIn")}>
-            Sign In
+            style={styles.RedirectToSignInText}
+            onPress={() => navigation.navigate("SignIn")}>
+              Sign In
           </Text>
         </Text>
         </View>
@@ -57,14 +92,59 @@ export default function SignUpScreen({ navigation }) {
   )
 }
 
-const Typeable = WhatToType => {
+const NameInput = props => {
   return (
-    <View style={styles.Typeable}>
+    <View style={styles.NameInput}>
       <TextInput
-        placeholder={`Enter your ${WhatToType.type}`}
-        onChangeText={WhatToType.onChangeText}
-        defaultValue={WhatToType.value}
-        keyboardType={WhatToType.type === "Email" 
+        style={styles.InputText}
+        placeholder={`Enter your ${props.type}`}
+        onChangeText={props.onChangeText}
+        defaultValue={props.value}
+        keyboardType={"default"}
+      />
+    </View>
+  )
+}
+
+const MaleButton = props => {
+  return (
+    <TouchableOpacity 
+      style={props.selected 
+        ? [styles.GenderButton, { backgroundColor: '#4a98e0' }]
+        : [styles.GenderButton, { backgroundColor: 'white' }]
+      }
+      onPress={props.onPress}>
+      <Text style={{ textAlign: 'center' }}>
+        Male
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const FemaleButton = props => {
+  return (
+    <TouchableOpacity 
+      style={props.selected 
+        ? [styles.GenderButton, { backgroundColor: '#fe6b75' }]
+        : [styles.GenderButton, { backgroundColor: 'white' }]
+      }
+      onPress={props.onPress}>
+      <Text style={{ textAlign: 'center' }}>
+        Female
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const StandardInput = props => {
+  return (
+    <View style={styles.StandardInput}>
+      <TextInput
+        style={styles.InputText}
+        placeholder={`Enter your ${props.type}`}
+        onChangeText={props.onChangeText}
+        defaultValue={props.value}
+        keyboardType={props.type === "Email" 
           ? "email-address"
           : "default"}
       />
@@ -72,16 +152,38 @@ const Typeable = WhatToType => {
   )
 }
 
-const SignUpButton = NextAction => {
-  const buttonPressed = () => {
-    console.log('Button pressed')
-  };
+const PasswordInput = props => {
   return (
-    <View style={styles.CreateEmailButton}>
-      <TouchableOpacity onPress={NextAction.onPress}>
+    <View>
+      <View style={styles.StandardInput}>
+        <TextInput
+          style={styles.InputText}
+          placeholder={`Enter your ${props.type}`}
+          onChangeText={props.onChangeText}
+          defaultValue={props.value}
+          keyboardType={'default'}
+          secureTextEntry={props.hidePassword}
+        />
+        <TouchableOpacity  onPress={()=> props.setHidePassword(prev => !prev)}>
+          <Image
+            style={styles.PasswordImage}
+            source={props.hidePassword 
+              ? require('../assets/PasswordShow.jpg')
+              : require('../assets/PasswordHide.webp')}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const SignUpButton = NextAction => {
+  return (
+      <TouchableOpacity 
+        style={styles.CreateEmailButton}
+        onPress={NextAction.onPress}>
         <Text style={styles.ButtonText}>Sign Up</Text>
       </TouchableOpacity>
-    </View>
   );
 };
 
@@ -97,22 +199,57 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  FormContainer: {
+    flexDirection: 'column',
+    paddingHorizontal: 20,
+    width: '100%',
+  },
   SignUpText: {
     fontSize: 24,
     marginBottom: 24,
     fontWeight: "bold",
     textAlign: "center"
   },
-  Typeable: {
-    width: '90%',
+  NameRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 6,
+    marginBottom: 12,
+  },
+  NameInput: {
+    flex: 1,
     borderWidth: 1,
     borderColor: "#aaa",
     borderRadius: 6,
-    marginBottom: 12,
     paddingHorizontal: 12,
     height: 48,
-    fontSize: 16,
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  GenderRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 6,
+    marginBottom: 12,
+  },
+  GenderButton: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#aaa",
+    borderRadius: 6,
+    paddingHorizontal: 12,
+    height: 48,
+    justifyContent: 'center',
+  },
+  StandardInput: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: "#aaa",
+    borderRadius: 6,
+    paddingHorizontal: 12,
+    height: 48,
+    marginBottom: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   CreateEmailButton: {
     backgroundColor: "#1E90FF",
@@ -121,7 +258,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 20,
     marginBottom: 16,
-    width: '90%'
   },
   ButtonText: {
     color: "#fff",
@@ -137,5 +273,13 @@ const styles = StyleSheet.create({
   linkContainer: {
     marginTop: 32,
     alignItems: "center"
-  }
+  },
+  PasswordImage: {
+    width: 32,
+    height: 32,
+  },
+  InputText: {
+    flex: 1, 
+    textAlign: 'left'
+  },
 });
