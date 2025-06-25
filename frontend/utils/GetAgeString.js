a550afc42cd858ee2c659f8cd1d11de478936ef3
William Liow <williamoose@Williams-MacBook-Pro.local>
2025-06-25 22:08:07 +0800

    one more

---
+++ frontend/utils/GetAgeString.js
@@ -0,0 +1,14 @@
+export default function getAgeString(dob) {
+    if (!dob) return "";
+
+    const today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    const dayDiff = today.getDate() - dob.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+      age--;
+    }
+
+    return `Age: ${age}`;
+  }
